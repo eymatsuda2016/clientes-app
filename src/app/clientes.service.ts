94eabf1599c507a9f19cb4877fdca7e9444d0250
eymatsuda2016 <eymatsuda2016@outlook.com>
2023-05-16 16:36:58 -0300

    Criado API Serviços Prestados e Salvar um Serviço Prestado

--- src/app/clientes.service.ts
+++ src/app/clientes.service.ts
@@ -27,5 +27,9 @@ export class ClientesService {
     return this.http.get<any>(`http://localhost:8080/api/clientes/${id}`);
   }
 
+  deletar(cliente : Cliente) : Observable<any>{
+    return this.http.delete<any> (`http://localhost:8080/api/clientes/${cliente.id}`);
+}
+
 }
 
